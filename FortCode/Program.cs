c0a3372a22627e9cc5edc0f031363a7ef8813e46
rajkumar-s311 <rajkumar.srk86@gmail.com>
2021-07-19 18:54:45 +0530

    Add files via upload

---
+++ FortCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace FortCode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CreateWebHostBuilder(args).Build().Run();
+           BuildWebHost(args).Run();
+        }
+
+        //    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //        WebHost.CreateDefaultBuilder(args)
+        //            .ConfigureAppConfiguration(((context, config) =>
+        //            {
+        //                config.AddEnvironmentVariables("FORTCODEENV_");
+        //            }))
+        //            .UseStartup<Startup>();
+        //}
+        public static IWebHost BuildWebHost(string[] args) =>
+                WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>()
+                    .Build();
+    }
+}
--- FortCode/Startup.cs
+++ FortCode/Startup.cs
@@ -1,1 +1,67 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using FortCode.Helpers;
 
+namespace FortCode
+{
+    public class Startup
+    {     
+        public virtual void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+             .AddJwtBearer(options => {
+                 options.TokenValidationParameters =
+                      new TokenValidationParameters
+                      {
+                          ValidateIssuer = true,
+                          ValidateAudience = true,
+                          ValidateLifetime = true,
+                          ValidateIssuerSigningKey = true,
+
+                          ValidIssuer = "FortCode",
+                          ValidAudience = "FortCode",
+                          IssuerSigningKey =JwtSecurityKey.Create("fortCode ")
+                      };
+             });
+
+            //services.AddAuthorization(options =>
+            //{
+            //    options.AddPolicy("Member",
+            //        policy => policy.RequireClaim("MembershipId"));
+            //});
+            //services.AddAuthentication();
+            services.AddAuthorization();
+
+            services
+                .AddMvc();
+
+            //services
+            //    .AddControllers();
+        }
+
+        public virtual void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            // app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+            app.UseAuthorization();
+             //app.UseMvcWithDefaultRoute();
+            app.UseEndpoints(endPoints => { endPoints.MapControllers(); });
+                //.UseFileServer()
+                //.UseRouting()
+                
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+
+
+        }
+    }
+}
